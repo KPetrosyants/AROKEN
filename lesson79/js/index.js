887d3daadfed43c49d2bb81410c23fcbb963ce2c
KPetrosyants <119255490+KPetrosyants@users.noreply.github.com>
2024-05-07 14:51:15 +1000

    Add files via upload

---
+++ lesson79/js/index.js
@@ -0,0 +1,21 @@
+// for (let i = 0; i < 11; i++) {
+//   if (!(i % 2)) {
+//     console.log(i);
+//   }
+// }
+// for (let i = 10; i > 0; i--) {
+//   console.log(i);
+// }
+
+let i = 0;
+while (i < 11) {
+  if (!(i % 2)) {
+    console.log(i);
+  }
+  i++;
+}
+let a = 10;
+while (a > 0) {
+  console.log(a);
+  i--;
+}
